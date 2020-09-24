84359a1ce0390ecafdf17328df2c6fe30b76f1b4
vanguyen777 <53541741+vanguyen777@users.noreply.github.com>
2020-09-24 16:56:07 -0400

    Update lab1.sh
    finish most of the stuffs, 
    need to add firewall configuration, "any other info" part
    need to write the script that connects acquired info and dish out the info

--- lab1.sh
+++ lab1.sh
@@ -6,8 +6,8 @@
 cat /etc/hostname
 
 #(b) disk/partition size, usage, and mount points
-#this command gets disk, partition mount point
-mount | grep -F "/dev/sda"
+#this command gets disk, partition names, size in 1M blocks, used, available, use% and mount point
+df -BM | grep -F "/dev/sda"
 
 
 #c) network devices (name)
@@ -20,28 +20,47 @@ ip a | grep -w "inet" | cut -d ' ' -f 6-8 | grep "brd"
 #this command also prints broadcast address for active IPv4 interfaces
 
 #(e) OS version/release level, kernel version used
+#this command shows OS version
+cat /etc/os-release | grep -w  "PRETTY_NAME"
+#this command shows Linux kernel version
+uname -r
 
 #(f) security mode (SELinux status)
-
 sestatus | head -1
+
 #(g) firewall configuration
+#user either firewall-cmd or iptables
 
 #(h) list of active repositories
+yum repolist
 
 #(i) number of software packages (rpm, deb, etc) installed
+yum list installed | wc -l
 
 #(j) name of software packages installed
+yum list installed 
 
 #k) dns server configured (which dns server(s) will be used for the local resolver)
+cat /etc/resolv.conf | grep -F "nameserver"
 
 #(l) list of active shell users
+w
 
 #(m) date the os was first installed
+rpm -qi setup | grep -F " Install Date"
 
 #(n) hardware details: cpu, main memory size, swap memory configure, devices, etc
+#cpu:
+lscpu
+#main memory size, swap memory config:
+free
+#devices:
+lspci
 
 #(o) services current running
+systemctl --type=service --state=running
 
 #(p) services installed but not running
+systemctl --type=service --state=exited
 
 #(q) any other information/data you think that is important and necessary to have.
